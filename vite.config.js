15652fedb37833afb212f5504cb78a1e15a7d20e
Valeria Lapshina <lapshina_valeria@mail.ru>
2025-05-16 22:15:57 +0300

    Конфиг поправлен для публикации

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const repositoryName = "WebProgramingLab3";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const base = command === "build" ? `/${repositoryName}/` : "/";
+  return {
+    plugins: [react()],
+    base: base,
+  };
+});
